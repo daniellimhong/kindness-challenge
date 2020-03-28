4d714115d153f2a934997ea3ba1eda67603a0727
Daniel <daniel@Daniels-MacBook-Pro-2.local>
2020-03-27 23:54:25 -0700

    updated browser router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LandingPage from './Routes/LandingPage.js';
 
 import './App.css';
@@ -7,13 +8,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/" component={LandingPage} />
-        {/* <Route path="/about" component={About} /> */}
-        {/* <Route path="/resources" component={ResourcesApp} /> */}
-        {/* <Route path="/addresource" component={AddResource} /> */}
-        {/* <Route path="/team" component={Team} /> */}
-      </Switch>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          {/* <Route path="/about" component={Inspiration} />
+          <Route path="/resources" component={ResourcesApp} />
+          <Route path="/team" component={Team} /> */}
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/Routes/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LandingPage = () => {
+
+    return (
+        <div>
+            Hello world
+        </div>
+    )
+}
+
+export default LandingPage;
