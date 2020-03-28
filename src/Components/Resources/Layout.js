fe51d049f9f63b9756f5955740346bef9c066c51
Austin Chabaud <austinchabaud@Austins-MacBook-Pro.local>
2020-03-28 17:02:28 -0500

    map styled

--- src/Components/Resources/Layout.js
+++ src/Components/Resources/Layout.js
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
 import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
+import styled from "styled-components";
+
+const mapStyle = {
+  height: "50%",
+  width: "100vw"
+};
 class GoogleMap extends Component {
   constructor(props) {
     super(props);
   }
   render() {
     const { google } = this.props;
-    console.log(process.env.REACT_APP_GOOGLE_KEY);
     return (
-      <div>
+      <div className="map-container">
         <Map
           google={google}
           zoom={10}
+          style={mapStyle}
           minZoom={2}
           maxZoom={15}
           initialCenter={{ lat: 40.712772, lng: -74.006058 }}
