f71d7de40d77c2bb6d5af0e5c736bec558e971b8
Austin Chabaud <austinchabaud@Austins-MacBook-Pro.local>
2020-03-29 15:55:59 -0500

    modal and stying

--- src/Routes/LandingPage.js
+++ src/Routes/LandingPage.js
@@ -1,24 +1,41 @@
 import React from "react";
 import NavBar from "../Components/Reusable/NavBar";
 import Footer from "../Components/Reusable/Footer";
-import background from '../assets/landing-bg.jpg'
-import styled from 'styled-components';
+import background from "../assets/landing-bg.jpg";
+import styled from "styled-components";
 
 const HeaderContainer = styled.div`
   width: 100%;
   height: 60vh;
-  background-image: linear-gradient(rgba(0, 0, 0, 0.705), rgba(0, 0, 0, 0.705)), url(${background});
+  background-image: linear-gradient(rgba(0, 0, 0, 0.705), rgba(0, 0, 0, 0.705)),
+    url(${background});
   background-size: cover;
   background-repeat: no-repeat;
-  background-position: center bottom
-`
+  background-position: center bottom;
+  .title-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: white;
+  }
+  h1 {
+    font-size: 50px;
+    font-family: "Architects Daughter", cursive;
+  }
+  h2 {
+    font-family: "Architects Daughter", cursive;
+  }
+`;
 
 const LandingPage = () => {
   return (
     <div>
       <NavBar />
       <HeaderContainer>
-
+        <div className="title-container">
+          <h1>The Kindness Challenge</h1>
+          <h2>A community driven effort to help those impacted by COVID-19</h2>
+        </div>
       </HeaderContainer>
       <Footer />
     </div>
