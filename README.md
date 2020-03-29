1d60b8bea46021ae5bff67cbc0be518e1c8b63a2
“Daniel” <daniellimhong@gmail.com>
2020-03-29 02:41:52 -0700

    quick styling before bed

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Description
 
 https://colorhunt.co/palette/154210
+
+https://unsplash.com/photos/DBu9IrkuPFM
--- src/Routes/LandingPage.js
+++ src/Routes/LandingPage.js
@@ -1,12 +1,25 @@
 import React from "react";
 import NavBar from "../Components/Reusable/NavBar";
 import Footer from "../Components/Reusable/Footer";
+import background from '../assets/landing-bg.jpg'
+import styled from 'styled-components';
+
+const HeaderContainer = styled.div`
+  width: 100%;
+  height: 60vh;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.705), rgba(0, 0, 0, 0.705)), url(${background});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center bottom
+`
 
 const LandingPage = () => {
   return (
     <div>
       <NavBar />
-      Home page though
+      <HeaderContainer>
+
+      </HeaderContainer>
       <Footer />
     </div>
   );
