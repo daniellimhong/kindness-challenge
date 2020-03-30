257d5305b53c7a370ab2acf4b52d7e0e8c8df2f9
Daniel <daniel@Daniels-MacBook-Pro-2.local>
2020-03-30 08:54:22 -0700

    configured react-router for gh deployment

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route exact path="/" component={LandingPage} />
           <Route path="/inspiration" component={Inspiration} />
