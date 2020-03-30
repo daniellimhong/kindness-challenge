ef766944430a4bb060c9003a876c92ce168bf507
“Daniel” <daniellimhong@gmail.com>
2020-03-30 06:47:48 -0700

    Updates

---
+++ precache-manifest.0d757cb37bc8d48ed3047a20818f20c6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f543e5cb85cdeeea80024d3bfddb233",
+    "url": "/kindness-challenge/index.html"
+  },
+  {
+    "revision": "85b3b477f6e5b1b1126e",
+    "url": "/kindness-challenge/static/css/main.d3c4f40c.chunk.css"
+  },
+  {
+    "revision": "439bf4bde3550a6dbfdd",
+    "url": "/kindness-challenge/static/js/2.6715f106.chunk.js"
+  },
+  {
+    "revision": "95c7175ec8238d28a4301f6bd59e2e9f",
+    "url": "/kindness-challenge/static/js/2.6715f106.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85b3b477f6e5b1b1126e",
+    "url": "/kindness-challenge/static/js/main.9c5cd12c.chunk.js"
+  },
+  {
+    "revision": "bec850dcfc9a455ac6bd",
+    "url": "/kindness-challenge/static/js/runtime-main.49b51486.js"
+  },
+  {
+    "revision": "3e960504917a719253ce6833fbdac502",
+    "url": "/kindness-challenge/static/media/austinpic.3e960504.png"
+  },
+  {
+    "revision": "c22b570242826e25a2d7682a98ba7682",
+    "url": "/kindness-challenge/static/media/daniel.c22b5702.png"
+  },
+  {
+    "revision": "9d423f3c51695c92711b7acfe28e2c43",
+    "url": "/kindness-challenge/static/media/inspiration.9d423f3c.jpg"
+  },
+  {
+    "revision": "3456ce4f397908eaf3cea5b4465321d4",
+    "url": "/kindness-challenge/static/media/landing.3456ce4f.jpg"
+  }
+]);
