ae668ec7e9bdb0fd5dde4f7f850246077ad50e81
Austin Chabaud <austinchabaud@Austins-MacBook-Pro.local>
2020-03-29 20:48:39 -0500

    latest commits

--- src/Components/Resources/ResourceCard.js
+++ src/Components/Resources/ResourceCard.js
@@ -1,28 +1,30 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 import { Container, Button, Input, Text } from "../../StyledComponents";
 
 const Card = styled.div`
-    border: 1px solid black;
-    // margin-top: 20px;
-    height: 250px;
-    width: 250px;
-    border-radius: 20px;
-    display: flex;
-    margin: 20px 10px 0 10px;
+  height: 15rem;
+  width: 20rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin: 20px;
+  background-image: linear-gradient(#ebedee, #fdfbfb);
+  flex-wrap: wrap;
+  border: 0.2rem solid lightgrey;
+  border-radius: 0.5rem;
+  box-shadow: 5px 2px 5px 0px lightgrey;
+`;
 
-`
-
-const ResourceCard = (props) => {
-
-    return (
-        <Card>
-            {props.resourceName}
-            {props.description}
-            {props.category}
-            {props.address}
-        </Card>
-    )
-}
+const ResourceCard = props => {
+  return (
+    <Card>
+      {props.resourceName}
+      {props.description}
+      {props.category}
+      {props.address}
+    </Card>
+  );
+};
 
 export default ResourceCard;
